f21e837915b792c4ca72367e571e2ccbdea78706
Akram <akramshafeek70@gmail.com>
2023-08-19 01:34:42 +0530

    Initiated project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import styles from './style'
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth} text-white`}>
+          Navbar
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth} text-white`}>
+          Hero
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth} text-white`}>
+          Stats
+          Business
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA
+          Footer
+        </div>
+      </div>
+    </div>
   )
 }
 
